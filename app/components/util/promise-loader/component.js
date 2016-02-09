a0280ad00183758279bef11a0ee62249888fcec1
Jerry Nummi <jerry@mutuallyhuman.com>
2016-02-09 13:44:57 -0500

    promise-loader test

--- app/components/util/promise-loader/component.js
+++ app/components/util/promise-loader/component.js
@@ -1,14 +1,15 @@
 import Ember from 'ember';
 import DS from 'ember-data';
 
-const { computed, isEmpty } = Ember;
+const { computed } = Ember;
 
 export default Ember.Component.extend({
   promise: null,
 
   promiseProxy: computed('promise', function() {
-    if (isEmpty(this.get('promise.then'))) { return null; }
-    return DS.PromiseObject.create({promise: this.get('promise')});
+    return DS.PromiseObject.create({
+      promise: Ember.RSVP.resolve(this.get('promise'))
+    });
   }),
 
   isFulfilled: computed.reads('promiseProxy.isFulfilled')
