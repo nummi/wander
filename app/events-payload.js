ff469d93188f25e4ee7dda806cf3aab22b83c1b4
Jerry Nummi <jerry@mutuallyhuman.com>
2016-02-09 11:36:57 -0500

    wip

--- app/events-payload.js
+++ app/events-payload.js
@@ -8,17 +8,61 @@
 **/
 
 export default {
-  "events": [
+  'events': [
     {
       id: 1,
-      title: "This is a title",
-      description: "This is a longer description here",
+      description: 'This is a long description here',
+      cost: 6.50,
       latitude: 39.96819355055,
       longitude: -82.996259488017,
-      venue: "V Power Yoga",
-      cost: 6.50,
+      venue: 'V Power Yoga',
       photos: [],
-      weatherPayload: {}
+      weather: {
+        'cloudcover': '75',
+        'FeelsLikeC': '-9',
+        'FeelsLikeF': '15',
+        'humidity': '63',
+        'observation_time': '07:06 PM',
+        'precipMM': '0.0',
+        'pressure': '1018',
+        'temp_C': '-3',
+        'temp_F': '27',
+        'visibility': '16',
+        'weatherCode': '116',
+        'weatherDesc': [ { 'value': 'Partly Cloudy' } ],
+        'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0002_sunny_intervals.png' } ],
+        'winddir16Point': 'ENE',
+        'winddirDegree': '60',
+        'windspeedKmph': '24',
+        'windspeedMiles': '15'
+      }
+    }, {
+      id: 2,
+      description: 'Cancelled flight, hotel provided by airline',
+      cost: 0.00,
+      latitude: 35.1591247,
+      longitude: -80.8869856,
+      venue: 'Hawthorn Suites by Wyndham',
+      photos: [],
+      weather: {
+        'cloudcover': '0',
+        'FeelsLikeC': '7',
+        'FeelsLikeF': '44',
+        'humidity': '27',
+        'observation_time': '10:49 PM',
+        'precipMM': '0.0',
+        'pressure': '1020',
+        'temp_C': '8',
+        'temp_F': '46',
+        'visibility': '16',
+        'weatherCode': '113',
+        'weatherDesc': [ { 'value': 'Clear' } ],
+        'weatherIconUrl': [ { 'value': 'http:\/\/cdn.worldweatheronline.net\/images\/wsymbols01_png_64\/wsymbol_0008_clear_sky_night.png' } ],
+        'winddir16Point': 'S',
+        'winddirDegree': '170',
+        'windspeedKmph': '7',
+        'windspeedMiles': '4'
+      }
     }
   ]
 }
