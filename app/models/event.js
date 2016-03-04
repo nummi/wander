6fc6116dc16805159423016678bbebc0b02b9d10
Jerry Nummi <jerry@mutuallyhuman.com>
2016-03-04 10:04:52 -0500

    Display vector weather icons

--- app/models/event.js
+++ app/models/event.js
@@ -2,7 +2,11 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 const { attr } = DS;
-const { computed, get } = Ember;
+const {
+  computed,
+  get,
+  isEmpty
+} = Ember;
 
 export default DS.Model.extend({
   cost: attr('dollars', { defaultValue: 0 }),
@@ -15,5 +19,16 @@ export default DS.Model.extend({
 
   displayText: computed('venue.name', 'description', function() {
     return get(this, 'venue.name') || get(this, 'description');
+  }),
+
+  iconPath: computed(function() {
+    const BASE = '/images/weather-icons/';
+    const urls = this.get('weather.weatherIconUrl');
+
+    if(isEmpty(urls)) {
+      return BASE + 'wsymbol_0999_unknown.svg';
+    }
+
+    return BASE + urls[0].value.replace('\.png', '.svg').match(/.*\/(.*?)$/)[1];
   })
 });
