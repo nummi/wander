3faafebc6c2afa4157010dea3ba20ccf2811c3d3
Jerry Nummi <jerry@mutuallyhuman.com>
2016-06-16 15:52:32 -0400

    JSON api endpoint namespace

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,6 @@ import ActiveModelAdapter from 'active-model-adapter';
 import ENV from 'wander/config/environment';
 
 export default ActiveModelAdapter.extend({
-  host: ENV.host
+  host: ENV.host,
+  namespace: 'api'
 });
